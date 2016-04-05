b4329d3b840a013b9764ce9613dba7ba0d058e5a
Felix Satyaputra <felix.satyaputra@typesafe.com>
2016-04-05 22:11:09 +1000

    HLists exercise

--- src/main/scala/learnshapeless/HLists.scala
+++ src/main/scala/learnshapeless/HLists.scala
@@ -33,8 +33,9 @@ object HLists extends App {
     (a) start the `sbt console`, and enter `:type learnshapeless.HLists.ex_newton`
     (b) if you are using Intellij, you can focus cursor on the expression and activate TypeInfo command (Ctrl-Shift-P on my Mac)
      */
-  def ex_newton = ???
-  println(s"ex_newton: $ex_newton")
+  def ex_newton: String :: Int :: Country :: HNil =
+    "Newton" :: 1642 :: England :: HNil
+  println(s"ex_newton $ex_newton")
 
   def eg_prependAndAppend: String :: String :: Int :: Country :: Discovery :: HNil = "Albert" +: eg1_einstein :+ TheoryOfRelativity
 
@@ -42,11 +43,16 @@ object HLists extends App {
   def ex_prependAndAppend = ???
   println(s"ex_prependAndAppend $ex_prependAndAppend")
 
+  /* Exercise 3: Prepend the String "Isaac" to `ex_newton` */
+  def ex_prepend: String :: String :: Int :: Country :: HNil =
+    "Isaac" :: ex_newton
+  println(s"ex_prepend $ex_prepend")
 
   /** Shapeless makes the link between tuples and HLists clear by offering all HList methods over tuples with the import below*/
   import shapeless.syntax.std.tuple._
   /* Convert `ex_newton` into a tuple */
-  def ex_tuple: (String, Int, Country) = ???
+  def ex_tuple: (String, Int, Country) =
+    ex_newton.tupled
   println(s"ex_tuple $ex_tuple")
 
   /* Using operations available via `import syntax.std.tuple._`, append `Calculus` to `ex_tuple`  */
@@ -56,7 +62,8 @@ object HLists extends App {
   def eg_from_tuple = ("Einstein", 1879, Germany).productElements
 
   /* convert ex_tuple_append into an HList */
-  def ex_from_tuple = ???
+  def ex_from_tuple =
+    ex_tuple :+ HNil
   println(s"ex_from_tuple $ex_from_tuple")
 
 
@@ -74,9 +81,22 @@ object HLists extends App {
 
   /* Apply a Poly1 mapping over `ex_newton` that converts the name to ALLCAPS, and leaves other fields unchanged
    * Return resulting HList */
-  def ex_poly = ???
-  println(s"ex_poly: $ex_poly")
+  object AllCapsPoly extends DefaultIdentityMapping {
+    implicit def allCaps = at[String](_.map(_.toUpper))
+  }
+  def ex_poly: String :: Int :: Country :: HNil =
+    ex_newton.map(AllCapsPoly)
+  println(s"ex_poly $ex_poly")
 
+  /* Try writing a Poly1 mapping like `isAustralian` above, that doesn't use a type parameter `C <: Country`. Instead
+   * directly define an `at[Country]` clause. Leave other fields unchanged.
+   * Apply your new mapping over `ex_newton` and return the result */
+  object IsAustralianPoly extends DefaultIdentityMapping {
+    implicit def isAustralia = at[Country](_ == Australia)
+  }
+
+  def ex_poly_country: String :: Int :: Boolean :: HNil =
+    ex_poly.map(IsAustralianPoly)
 
 
   /* Note how exact type of element 2 is returned */
@@ -85,6 +105,11 @@ object HLists extends App {
   /* Exercise: what happen if you try to index an HList outside it bounds? Try accessing the 4th element of `eg1_einstein` */
   def ex_indexOutOfBounds = ???
 
+  /* Extract the 3rd element of `ex_poly_country` using a index.
+  Does the result surprise you? Why did it happen this way? */
+  def ex_poly_country_element_3rd: Boolean =
+    ex_poly_country(2)
+  println(s"ex_poly_country_element_3rd $ex_poly_country_element_3rd")
 
   def eg_mapped_by_index = eg1_einstein.updateAtWith(2)(_ == Australia)
 
@@ -92,7 +117,8 @@ object HLists extends App {
   Because `Data.scientistsFirstNames` is a `Map`, it can be passed as a function K => V
   * Use updateAtWith to identify the field by numeric index rather than y type.
   * `updateAtWith` returns the old value, and the updated HList in a tuple */
-  def ex_to_firstname_by_index: (String, HList) = ???
+  def ex_to_firstname_by_index: (String, HList) =
+    ex_newton.updateAtWith(0)(Data.scientistsFirstNames.getOrElse(_, "Unknown"))
   println(s"ex_to_firstname_by_index $ex_to_firstname_by_index")
 
 }
